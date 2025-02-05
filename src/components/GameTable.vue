09278fe15c7ca15b3bfb094d4c372aea882dea2f
Calle Svensson <calle.svensson@zeta-two.com>
2025-02-05 20:54:54 +0100

    Change team table layout

--- src/components/GameTable.vue
+++ src/components/GameTable.vue
@@ -14,7 +14,7 @@ const gamesStore = useGamesStore()
       v-else
       v-for="(game, index) in gamesStore.games"
       :key="game.id"
-      class="w-full max-w-4xl p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 relative"
+      class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 relative"
       :is="game.isDummy() ? DummyGame : ParsedGame"
       :game="game"
       :index="index"
--- src/components/GameTeam.vue
+++ src/components/GameTeam.vue
@@ -5,22 +5,47 @@ import type { Team } from '@/entities/game'
 
 const { team } = defineProps<{
   team: Team
+  position: 'left' | 'right'
 }>()
 </script>
 
 <template>
-  <div class="border-4 p-6 rounded-lg shadow-lg flex flex-col justify-stretch">
-    <div v-for="player in team.players" :key="player.profile" class="flex items-center gap-4">
-      <PlayerColor :color="player.color ?? 0" class="w-9 h-9" />
-      <div class="w-[150px] text-center overflow-hidden text-ellipsis font-medium dark:text-white">
-        <a
-          :href="`https://aoe2insights.com/user/relic/${player.profile}`"
-          class="text-blue-500 dark:text-blue-400 hover:underline"
-          target="_blank"
-          >{{ player.name }}</a
-        >
+  <div class="border-2 p-6 rounded-lg flex flex-col justify-stretch">
+    <template v-if="position == 'left'">
+      <div
+        v-for="player in team.players"
+        :key="player.profile"
+        class="flex items-center justify-end gap-4"
+      >
+        <div class="grow text-center overflow-hidden text-ellipsis font-medium dark:text-white">
+          <a
+            :href="`https://aoe2insights.com/user/relic/${player.profile}`"
+            class="text-blue-500 dark:text-blue-400 hover:underline"
+            target="_blank"
+            >{{ player.name }}</a
+          >
+        </div>
+        <CivIcon :civ="player.civ.toLowerCase()" class="w-10 h-10 rounded-full" />
+        <PlayerColor :color="player.color ?? 0" class="w-9 h-9" />
       </div>
-      <CivIcon :civ="player.civ.toLowerCase()" class="w-10 h-10 rounded-full" />
-    </div>
+    </template>
+    <template v-if="position == 'right'">
+      <div
+        v-for="player in team.players"
+        :key="player.profile"
+        class="flex justify-start items-center gap-4"
+      >
+        <PlayerColor :color="player.color ?? 0" class="w-9 h-9" />
+        <CivIcon :civ="player.civ.toLowerCase()" class="w-10 h-10 rounded-full" />
+        <div class="grow text-center overflow-hidden text-ellipsis font-medium dark:text-white">
+          <a
+            :href="`https://aoe2insights.com/user/relic/${player.profile}`"
+            class="text-blue-500 dark:text-blue-400 hover:underline"
+            target="_blank"
+            >{{ player.name }}</a
+          >
+        </div>
+      </div>
+    </template>
   </div>
 </template>
